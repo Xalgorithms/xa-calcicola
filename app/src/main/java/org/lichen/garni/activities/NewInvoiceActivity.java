036ddde077602d85f6b341155bc5d2c3a338b44a
Don Kelly <karfai@gmail.com>
2015-11-29 15:26:58 -0500

    Start building new invoice activity

--- app/src/main/java/org/lichen/garni/activities/NewInvoiceActivity.java
+++ app/src/main/java/org/lichen/garni/activities/NewInvoiceActivity.java
@@ -1,14 +1,42 @@
 package org.lichen.garni.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import org.lichen.garni.R;
 
 public class NewInvoiceActivity extends RxActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_invoice);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m) {
+        MenuInflater mi = getMenuInflater();
+        mi.inflate(R.menu.actions_new_invoice, m);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem m) {
+        switch (m.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            case R.id.action_new_invoice_save:
+                save();
+                return true;
+        }
+        return super.onOptionsItemSelected(m);
+    }
+
+    private void save() {
+        finish();
     }
 }
