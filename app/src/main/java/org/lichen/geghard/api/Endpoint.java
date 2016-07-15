177614b92435701f403a7ec48a2db9815729b68b
Don Kelly <karfai@gmail.com>
2016-07-15 00:47:00 -0400

    More API

--- app/src/main/java/org/lichen/geghard/api/Endpoint.java
+++ app/src/main/java/org/lichen/geghard/api/Endpoint.java
@@ -2,13 +2,21 @@ package org.lichen.geghard.api;
 
 import java.util.List;
 
+import retrofit.http.Body;
 import retrofit.http.GET;
+import retrofit.http.POST;
 import retrofit.http.Path;
 import rx.Observable;
 
 public interface Endpoint {
     @GET("/api/v1/invoices/{id}")
     Observable<Invoice> invoice(@Path("id") int id);
+
+    @POST("/api/v1/invoices")
+    Observable<Invoice> create_invoice(@Body Invoice invoice);
+
+    @GET("/api/v1/accounts")
+    Observable<List<Account>> accounts();
 
     @GET("/api/v1/accounts/{id}")
     Observable<Account> account(@Path("id") int id);
