aa643ef0a5043a8e4a1566c21d0b87cd03a779c0
Don Kelly <karfai@gmail.com>
2016-07-15 00:46:57 -0400

    Initial project generation

---
+++ app/src/androidTest/java/org/lichen/garni/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.lichen.garni;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
